37539b3544eef0481887323282943437e54ebe62
melany-eshuis <melany.eshuis@gmail.com>
2021-10-14 21:52:03 +0200

    Cleaning up the JS

--- indexv2.js
+++ indexv2.js
@@ -1,56 +1,52 @@
-function kortsteRoute() {
-  const begin = document.getElementById("begin").value;
-  const eind = document.getElementById("eind").value;
-  const adress = "http://localhost:8082/getpath/" + begin + "/" + eind;
+function kortsteRoute() 
+{
+	const begin = document.getElementById("begin").value;
+	const eind = document.getElementById("eind").value;
+	const adress = "http://localhost:8082/getpath/" + begin + "/" + eind;
 
-  console.log("Adress is:");
-  console.log(adress);
-
-  const xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      const div = document.getElementById("resultaat");
-      div.innerHTML = "<p>" + xhr.responseText + "</p>";
-    }
-  };
-
-  //console.log(this.responseText);
-
-  xhr.open("GET", adress, true);
-  xhr.send();
+	const xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function() 
+	{
+		if(this.readyState == 4 && this.status == 200) 
+		{
+			const div = document.getElementById("resultaat");
+			div.innerHTML = "<p>" + xhr.responseText + "</p>";
+		}
+	};
+	
+	xhr.open("GET", adress, true);
+	xhr.send();
 }
 
-function initialize() {
-    const xhr = new XMLHttpRequest();
-    
+function initialize() 
+{
+	const xhr = new XMLHttpRequest();
   
-  xhr.open("GET", "http://localhost:8082/initialize", true);
-  xhr.send();
+	xhr.open("GET", "http://localhost:8082/initialize", true);
+	xhr.send();
 }
 
 var allStars = [];
 
-function courierservice() {
-  const xhr = new XMLHttpRequest();
-  console.log("TESTING")
+function courierservice() 
+{
+	const xhr = new XMLHttpRequest();
+	console.log("TESTING")
 
-  xhr.onreadystatechange = function () {
-
-    if (this.readyState == 4 && this.status == 200) {
-      allStars = JSON.parse(this.responseText)
-      var result = document.getElementById("resultaat");
-      result.innerHTML = allStars[0].x +" "+ allStars[0].y +" "+ allStars[0].hex +" "+ allStars[0].r;
-      
-      
-    } 
-  }
-xhr.open("GET", "http://localhost:8082/courierservice", true);
-xhr.send();
-  
+	xhr.onreadystatechange = function() 
+	{
+		if(this.readyState == 4 && this.status == 200) 
+		{
+			allStars = JSON.parse(this.responseText);    
+		} 
+	}
+	xhr.open("GET", "http://localhost:8082/courierservice", true);
+	xhr.send();
 }
 
-function randomSign() {
-  return Math.random() >= 0.5 ? 1 : -1;
+function randomSign() 
+{
+	return Math.random() >= 0.5 ? 1 : -1;
 }
 
 const mapCanvas = document.getElementById("galaxyMap");
@@ -58,110 +54,98 @@ const mapContext = mapCanvas.getContext("2d");
 const starCanvas = document.getElementById("stars");
 const starsCTX = starCanvas.getContext("2d");
 
-function drawStar(star, mapRatio) {
-  mapContext.beginPath();
+function drawStar(star, mapRatio) 
+{
+	mapContext.beginPath();
   //x,y,radius,0,2*PI
-  var xMultiplier = mapCanvas.width/48
-  var yMultiplier = mapCanvas.width/27
+	var xMultiplier = mapCanvas.width/48;
+	var yMultiplier = mapCanvas.width/27;
 
+	mapContext.arc(
+		star.x *  xMultiplier,
+		star.y *  yMultiplier /2,
+		star.r * mapRatio,
+		0,
+		2 * Math.PI
+	);
   
-
-
-  mapContext.arc(
-    star.x *  xMultiplier,
-    star.y *  yMultiplier /2,
-    star.r * mapRatio,
-    0,
-    2 * Math.PI
-  );
-  
-  mapContext.fillStyle = star.hex;
-  mapContext.fill();
+	mapContext.fillStyle = star.hex;
+	mapContext.fill();
 }
 
-function drawRaster(mapRatio){
-  //draw horizontal lines
-  for( i=0; i<100; i++){
-    var multiplier = mapCanvas.width/48
-    mapContext.beginPath();
-    mapContext.moveTo(multiplier *i, 0)
-    mapContext.lineTo(multiplier *i, mapCanvas.width)
-    mapContext.strokeStyle = "#C8C8C8";
-    mapContext.lineWidth = 1 * mapRatio;
-    mapContext.stroke();
-    
-  }
+function drawRaster(mapRatio)
+{
+	//draw horizontal lines
+	for( i=0; i<100; i++)
+	{
+		var multiplier = mapCanvas.width/48;
+		mapContext.beginPath();
+		mapContext.moveTo(multiplier *i, 0);
+		mapContext.lineTo(multiplier *i, mapCanvas.width);
+		mapContext.strokeStyle = "#C8C8C8";
+		mapContext.lineWidth = 1 * mapRatio;
+		mapContext.stroke();
+	}
+	
   //draw vertical lines
-  for( i=0; i<100; i++){
-    var multiplier = mapCanvas.height/27;
-    mapContext.beginPath();
-    mapContext.moveTo(0,multiplier *i);
-    mapContext.lineTo(mapCanvas.width,multiplier *i);
-    mapContext.strokeStyle = "#C8C8C8";
-    mapContext.lineWidth =1 * mapRatio;
-    mapContext.stroke();
-    
-  }
+	for( i=0; i<100; i++)
+	{
+		var multiplier = mapCanvas.height/27;
+		mapContext.beginPath();
+		mapContext.moveTo(0,multiplier *i);
+		mapContext.lineTo(mapCanvas.width,multiplier *i);
+		mapContext.strokeStyle = "#C8C8C8";
+		mapContext.lineWidth =1 * mapRatio;
+		mapContext.stroke();
+	}
 }
 
-function initSpaceRaster() {
-  const rect = mapCanvas.getBoundingClientRect();
-  mapCanvas.width = rect.width * devicePixelRatio;
-  mapCanvas.height = rect.height * devicePixelRatio;
-  mapContext.scale(devicePixelRatio, devicePixelRatio);
-  const mapRatio = mapCanvas.width / 1920;
+function initSpaceRaster() 
+{
+	const rect = mapCanvas.getBoundingClientRect();
+	mapCanvas.width = rect.width * devicePixelRatio;
+	mapCanvas.height = rect.height * devicePixelRatio;
+	mapContext.scale(devicePixelRatio, devicePixelRatio);
+	const mapRatio = mapCanvas.width / 1920;
 
-  drawRaster(mapRatio)
-  
-  // const stars = [ 
-  //   {x: 9 , y: 11, r: 20, color: "#FFFFFF" },
-  //   {x: 10 , y: 12, r: 20, color: "#FFFFFF"},
-  //   {x: 11 , y: 13, r: 20, color: "red"},
-  //   {x: 5 , y: 11, r: 20, color: "blue"},
-  //   {x: 6 , y: 11.5, r: 20, color: "green"},
-  //   {x: 7 , y: 12, r: 20, color: "red"}]
-
-  // stars.forEach((star) => {
-  //   drawStar(star, mapRatio);
-  // })
-
-
-
-
+	drawRaster(mapRatio);
 }
 
-function initStars() {
-  const COLOR_SPACE = "black";
-  const COLOR_STARS = "white";
-  const STAR_NUM = 200;
-  const STAR_SIZE = 0.005;
-  const STAR_SPEED = 0.05;
-  const rect2 = starCanvas.getBoundingClientRect();
-  starCanvas.width = rect2.width * devicePixelRatio;
-  starCanvas.height = rect2.height * devicePixelRatio;
-  starsCTX.scale(devicePixelRatio, devicePixelRatio);
-  const stars = [];
-  const starSpeed = STAR_SPEED * starCanvas.width;
-  const xv = starSpeed * randomSign() * Math.random();
-  const yv = Math.sqrt(Math.pow(starSpeed, 2) - Math.pow(xv, 2)) * randomSign();
+function initStars() 
+{
+	const COLOR_SPACE = "black";
+	const COLOR_STARS = "white";
+	const STAR_NUM = 200;
+	const STAR_SIZE = 0.005;
+	const STAR_SPEED = 0.05;
+	const rect2 = starCanvas.getBoundingClientRect();
+	starCanvas.width = rect2.width * devicePixelRatio;
+	starCanvas.height = rect2.height * devicePixelRatio;
+	starsCTX.scale(devicePixelRatio, devicePixelRatio);
+	const stars = [];
+	const starSpeed = STAR_SPEED * starCanvas.width;
+	const xv = starSpeed * randomSign() * Math.random();
+	const yv = Math.sqrt(Math.pow(starSpeed, 2) - Math.pow(xv, 2)) * randomSign();
 
-  for (let i = 0; i < STAR_NUM; i++) {
-    const speedMult = Math.random() * 1.5 + 0.5;
-    stars[i] = {
-      r: (Math.random() * STAR_SIZE * starCanvas.width) / 2,
-      x: Math.floor(Math.random() * starCanvas.width),
-      y: Math.floor(Math.random() * starCanvas.height),
-      xv: xv * speedMult,
-      yv: yv * speedMult,
-    };
-  }
+	for (let i = 0; i < STAR_NUM; i++) 
+	{
+		const speedMult = Math.random() * 1.5 + 0.5;
+		stars[i] = 
+		{
+			r: (Math.random() * STAR_SIZE * starCanvas.width) / 2,
+			x: Math.floor(Math.random() * starCanvas.width),
+			y: Math.floor(Math.random() * starCanvas.height),
+			xv: xv * speedMult,
+			yv: yv * speedMult,
+		};
+	}
 
-  let timeDelta,
-    timeLast = 0;
-  requestAnimationFrame(loop);
+	let timeDelta, timeLast = 0;
+	requestAnimationFrame(loop);
 
-  // set up the animation loop
-  function loop(timeNow) {
+	// set up the animation loop
+	function loop(timeNow) 
+	{
     // calcualte the time difference
     timeDelta = timeNow - timeLast;
     timeLast = timeNow;
@@ -173,43 +157,53 @@ function initStars() {
 
     //draw the stars
     starsCTX.fillStyle = COLOR_STARS;
-    for (let i = 0; i < STAR_NUM; i++) {
-      starsCTX.beginPath();
-      starsCTX.arc(stars[i].x, stars[i].y, stars[i].r, 0, Math.PI * 2);
-      starsCTX.fill();
+    for (let i = 0; i < STAR_NUM; i++) 
+	{
+		starsCTX.beginPath();
+		starsCTX.arc(stars[i].x, stars[i].y, stars[i].r, 0, Math.PI * 2);
+		starsCTX.fill();
 
-      // update the star's x position
-      stars[i].x += stars[i].xv * timeDelta * 0.001;
+		// update the star's x position
+		stars[i].x += stars[i].xv * timeDelta * 0.001;
 
-      // reposition the star to the other side if it goes off screen
-      if (stars[i].x < 0 - stars[i].r) {
-        stars[i].x = starCanvas.width + stars[i].r;
-      } else if (stars[i].x > starCanvas.width + stars[i].r) {
-        stars[i].x = 0 - stars[i].r;
-      }
+		// reposition the star to the other side if it goes off screen
+		if (stars[i].x < 0 - stars[i].r) 
+		{
+			stars[i].x = starCanvas.width + stars[i].r;
+		} 
+		else if (stars[i].x > starCanvas.width + stars[i].r) 
+		{
+			stars[i].x = 0 - stars[i].r;
+		}
 
-      // update the star's y position
-      stars[i].y += stars[i].yv * timeDelta * 0.001;
+		// update the star's y position
+		stars[i].y += stars[i].yv * timeDelta * 0.001;
 
-      // reposition the star to the other side if it goes off screen
-      if (stars[i].y < 0 - stars[i].r) {
-        stars[i].y = starCanvas.height + stars[i].r;
-      } else if (stars[i].y > starCanvas.height + stars[i].r) {
-        stars[i].y = 0 - stars[i].r;
-      }
+		// reposition the star to the other side if it goes off screen
+		if (stars[i].y < 0 - stars[i].r) 
+		{
+		stars[i].y = starCanvas.height + stars[i].r;
+		} 
+		else if (stars[i].y > starCanvas.height + stars[i].r) 
+		{
+			stars[i].y = 0 - stars[i].r;
+		}
     }
 
     //call the next frame
     requestAnimationFrame(loop);
-  }
+	}
 }
 
 
-function smort(){
-  for(i=0; i< allStars.length; i++){
-    drawStar(allStars[i],(mapCanvas.width / 1920))
-  }
+function smort()
+{
+	for(i=0; i< allStars.length; i++)
+	{
+		drawStar(allStars[i],(mapCanvas.width / 1920));
+	}
 }
+
 //initialize();
 courierservice();
 //initGalaxy();
